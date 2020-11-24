8080e1fe506cfdb0e208dce77a01f3fe71daab73
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2020-11-23 23:46:26 -0500

    Added system traits. Cleaned up main game loop by hiding the processing logic of pygame events inside of PygameHeroControlSystem

--- rogue/cli/rogue_pygcurse.py
+++ rogue/cli/rogue_pygcurse.py
@@ -1,68 +1,37 @@
 import pathlib
 
-import pygame
-from toolz import first
-
 from rogue.io.loaders import load_rogue_ecdb_from_input_yaml
-from rogue.generic.ecs import (
-    add_system,
-    add_component,
-    query_entities,
-    create_systems,
-)
+from rogue.generic.ecs import create_systems, add_system
 from rogue.systems import (
     Systems,
     SystemUnion,
     process_systems,
-    create_pygcurse_render_system,
-    create_movement_system,
-    create_enemy_ai_system,
+    PygcurseRenderSystem,
+    MovementSystem,
+    EnemyAISystem,
+    PygameHeroControlSystem,
+    SystemFeedback,
 )
-from rogue.components import create_velocity_component
 
-from rogue.query_functions import is_hero
+
+def create_rogue_systems(*, window_height: int, window_width: int) -> Systems[SystemUnion]:
+
+    systems: Systems[SystemUnion] = create_systems()
+    systems = add_system(systems=systems, priority=0, system=EnemyAISystem.create())
+    systems = add_system(systems=systems, priority=1, system=MovementSystem.create())
+    systems = add_system(
+        systems=systems, priority=2, system=PygcurseRenderSystem.create(height=window_height, width=window_width)
+    )
+    systems = add_system(systems=systems, priority=0, system=PygameHeroControlSystem.create())
+    return systems
 
 
 def rogue_pygcurse(*, input_file_name: pathlib.Path, window_height: int, window_width: int) -> None:
 
     ecdb = load_rogue_ecdb_from_input_yaml(input_file_name=input_file_name)
-
-    systems: Systems[SystemUnion] = create_systems()
-    systems = add_system(systems=systems, priority=0, system=create_enemy_ai_system())
-    systems = add_system(systems=systems, priority=1, system=create_movement_system())
-    systems = add_system(
-        systems=systems, priority=2, system=create_pygcurse_render_system(height=window_height, width=window_width)
-    )
-
-    hero = first(query_entities(ecdb=ecdb, query_function=is_hero))
+    systems = create_rogue_systems(window_height=window_height, window_width=window_width)
 
     while True:
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-
-            if event.type == pygame.KEYDOWN:
-                hero_velocity_y, hero_velocity_x = 0, 0
-
-                if event.key == pygame.K_LEFT:
-                    hero_velocity_x = -1
-                elif event.key == pygame.K_RIGHT:
-                    hero_velocity_x = 1
-                elif event.key == pygame.K_UP:
-                    hero_velocity_y = -1
-                elif event.key == pygame.K_DOWN:
-                    hero_velocity_y = 1
-                elif event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    return
-                else:
-                    continue
-
-                ecdb = add_component(
-                    ecdb=ecdb,
-                    entity=hero,
-                    component=create_velocity_component(y_axis=hero_velocity_y, x_axis=hero_velocity_x),
-                )
-                ecdb = process_systems(ecdb=ecdb, systems=systems)
+        ecdb, feedback = process_systems(ecdb=ecdb, systems=systems)
+        if feedback == SystemFeedback.QuitGame:
+            return
--- rogue/systems.py
+++ rogue/systems.py
@@ -1,11 +1,15 @@
+import abc
+import enum
 import random
 import typing
 
 import attr
+import pygame
 import pygcurse
+from toolz import first
 
 from rogue.generic.functions import evolve
-from rogue.query_functions import is_enemy
+from rogue.query_functions import is_hero, is_enemy
 from rogue.generic.ecs import (
     EntityComponentDatabase,
     Systems,
@@ -26,171 +30,243 @@ from rogue.components import (
 from rogue.components import create_velocity_component
 
 
-# Systems
+class SystemFeedback(enum.Enum):
+    NoFeedback = enum.auto()
+    IgnorePygameEvent = enum.auto()
+    QuitGame = enum.auto()
+
+
 @attr.s(frozen=True, kw_only=True)
-class PygcurseRenderSystem:
+class DoNotChangeEntityComponentDatabaseTrait(abc.ABC):
+    @abc.abstractmethod
+    def __call__(self, *, ecdb: EntityComponentDatabase[RogueComponentUnion]) -> SystemFeedback:
+        ...
+
+
+@attr.s(frozen=True, kw_only=True)
+class ReturnEntityComponentDatabaseTrait:
+    @abc.abstractmethod
+    def __call__(
+        self, *, ecdb: EntityComponentDatabase[RogueComponentUnion]
+    ) -> typing.Tuple[EntityComponentDatabase[RogueComponentUnion], SystemFeedback]:
+        ...
+
+
+@attr.s(frozen=True, kw_only=True)
+class YieldEntityComponentDatabaseTrait:
+    @abc.abstractmethod
+    def __call__(
+        self, *, ecdb: EntityComponentDatabase[RogueComponentUnion]
+    ) -> typing.Generator[typing.Tuple[EntityComponentDatabase[RogueComponentUnion], SystemFeedback], None, None]:
+        ...
+
+
+@attr.s(frozen=True, kw_only=True)
+class PygcurseRenderSystem(DoNotChangeEntityComponentDatabaseTrait):
     window: pygcurse.PygcurseWindow = attr.ib()
 
+    @classmethod
+    def create(cls, *, height: int, width: int) -> "PygcurseRenderSystem":
+        window = pygcurse.PygcurseWindow(width=width, height=height)
+        return PygcurseRenderSystem(window=window)
 
-@attr.s(frozen=True, kw_only=True)
-class MovementSystem:
-    ...
-
-
-@attr.s(frozen=True, kw_only=True)
-class EnemyAISystem:
-    ...
-
-
-SystemUnion = typing.Union[PygcurseRenderSystem, MovementSystem, EnemyAISystem]
-# Systems End
-
-
-def create_pygcurse_render_system(*, height: int, width: int) -> PygcurseRenderSystem:
-    window = pygcurse.PygcurseWindow(width=width, height=height)
-    return PygcurseRenderSystem(window=window)
-
-
-def create_movement_system() -> MovementSystem:
-    return MovementSystem()
-
-
-def create_enemy_ai_system() -> EnemyAISystem:
-    return EnemyAISystem()
-
-
-def process_pygcurse_render_system(
-    *, system: PygcurseRenderSystem, ecdb: EntityComponentDatabase[RogueComponentUnion]
-) -> None:
-    for entity in get_entities(ecdb=ecdb):
-        position_component = typing.cast(
-            typing.Optional[PositionComponent],
-            get_component_of_entity(ecdb=ecdb, entity=entity, component_type=PositionComponent),
-        )
-        assert position_component is not None
-
-        size_component = typing.cast(
-            typing.Optional[SizeComponent],
-            get_component_of_entity(ecdb=ecdb, entity=entity, component_type=SizeComponent),
-        )
-        if size_component is not None:
-
-            height, width = size_component.height, size_component.width
-
-            # Left Wall
-            start_pos = position_component.x_axis, position_component.y_axis + 1
-            end_pos = position_component.x_axis, position_component.y_axis + height - 1
-            system.window.drawline(char="|", start_pos=start_pos, end_pos=end_pos, fgcolor="grey")
-
-            # Right Wall
-            start_pos = position_component.x_axis + width, position_component.y_axis + 1
-            end_pos = position_component.x_axis + width, position_component.y_axis + height - 1
-            system.window.drawline(char="|", start_pos=start_pos, end_pos=end_pos, fgcolor="grey")
-
-            # Top Wall
-            start_pos = position_component.x_axis, position_component.y_axis
-            end_pos = position_component.x_axis + width, position_component.y_axis
-            system.window.drawline(char="_", start_pos=start_pos, end_pos=end_pos, fgcolor="grey")
-
-            # Bottom Wall
-            start_pos = position_component.x_axis, position_component.y_axis + height
-            end_pos = position_component.x_axis + width, position_component.y_axis + height
-            system.window.drawline(char="_", start_pos=start_pos, end_pos=end_pos, fgcolor="grey")
-
-            # Inside of the Room
-            y_axis = position_component.y_axis + 1
-            x_axis = position_component.x_axis + 1
-            system.window.fill(char=".", region=(x_axis, y_axis, width - 1, height - 1), fgcolor="grey")
-
-    for entity in get_entities(ecdb=ecdb):
-
-        position_component = typing.cast(
-            typing.Optional[PositionComponent],
-            get_component_of_entity(ecdb=ecdb, entity=entity, component_type=PositionComponent),
-        )
-        assert position_component is not None
-
-        appearance_component = typing.cast(
-            typing.Optional[AppearanceComponent],
-            get_component_of_entity(ecdb=ecdb, entity=entity, component_type=AppearanceComponent),
-        )
-        if appearance_component is not None:
-            system.window.putchar(
-                appearance_component.symbol,
-                x=position_component.x_axis,
-                y=position_component.y_axis,
-                fgcolor=appearance_component.color,
+    def __call__(self, *, ecdb: EntityComponentDatabase[RogueComponentUnion]) -> SystemFeedback:
+        for entity in get_entities(ecdb=ecdb):
+            position_component = typing.cast(
+                typing.Optional[PositionComponent],
+                get_component_of_entity(ecdb=ecdb, entity=entity, component_type=PositionComponent),
             )
+            assert position_component is not None
+
+            size_component = typing.cast(
+                typing.Optional[SizeComponent],
+                get_component_of_entity(ecdb=ecdb, entity=entity, component_type=SizeComponent),
+            )
+            if size_component is not None:
+
+                height, width = size_component.height, size_component.width
+
+                # Left Wall
+                start_pos = position_component.x_axis, position_component.y_axis + 1
+                end_pos = position_component.x_axis, position_component.y_axis + height - 1
+                self.window.drawline(char="|", start_pos=start_pos, end_pos=end_pos, fgcolor="grey")
+
+                # Right Wall
+                start_pos = position_component.x_axis + width, position_component.y_axis + 1
+                end_pos = position_component.x_axis + width, position_component.y_axis + height - 1
+                self.window.drawline(char="|", start_pos=start_pos, end_pos=end_pos, fgcolor="grey")
+
+                # Top Wall
+                start_pos = position_component.x_axis, position_component.y_axis
+                end_pos = position_component.x_axis + width, position_component.y_axis
+                self.window.drawline(char="_", start_pos=start_pos, end_pos=end_pos, fgcolor="grey")
+
+                # Bottom Wall
+                start_pos = position_component.x_axis, position_component.y_axis + height
+                end_pos = position_component.x_axis + width, position_component.y_axis + height
+                self.window.drawline(char="_", start_pos=start_pos, end_pos=end_pos, fgcolor="grey")
+
+                # Inside of the Room
+                y_axis = position_component.y_axis + 1
+                x_axis = position_component.x_axis + 1
+                self.window.fill(char=".", region=(x_axis, y_axis, width - 1, height - 1), fgcolor="grey")
+
+        for entity in get_entities(ecdb=ecdb):
+
+            position_component = typing.cast(
+                typing.Optional[PositionComponent],
+                get_component_of_entity(ecdb=ecdb, entity=entity, component_type=PositionComponent),
+            )
+            assert position_component is not None
+
+            appearance_component = typing.cast(
+                typing.Optional[AppearanceComponent],
+                get_component_of_entity(ecdb=ecdb, entity=entity, component_type=AppearanceComponent),
+            )
+            if appearance_component is not None:
+                self.window.putchar(
+                    appearance_component.symbol,
+                    x=position_component.x_axis,
+                    y=position_component.y_axis,
+                    fgcolor=appearance_component.color,
+                )
+
+        return SystemFeedback.NoFeedback
 
 
-def process_movement_system(
-    *, system: MovementSystem, ecdb: EntityComponentDatabase[RogueComponentUnion]
-) -> EntityComponentDatabase[RogueComponentUnion]:
-    assert system  # Hack to make 'system' variable marked as used
+@attr.s(frozen=True, kw_only=True)
+class MovementSystem(ReturnEntityComponentDatabaseTrait):
+    ZERO_VELOCITY_COMPONENT = create_velocity_component(x_axis=0, y_axis=0)
 
-    for entity in get_entities_with_component(ecdb=ecdb, component_type=VelocityComponent):
+    @classmethod
+    def create(cls) -> "MovementSystem":
+        return cls()
 
-        position_component = typing.cast(
-            typing.Optional[PositionComponent],
-            get_component_of_entity(ecdb=ecdb, entity=entity, component_type=PositionComponent),
-        )
-        assert position_component is not None
+    def __call__(
+        self, *, ecdb: EntityComponentDatabase[RogueComponentUnion]
+    ) -> typing.Tuple[EntityComponentDatabase[RogueComponentUnion], SystemFeedback]:
 
-        velocity_component = typing.cast(
-            typing.Optional[VelocityComponent],
-            get_component_of_entity(ecdb=ecdb, entity=entity, component_type=VelocityComponent),
-        )
-        assert velocity_component is not None
+        for entity in get_entities_with_component(ecdb=ecdb, component_type=VelocityComponent):
+            position_component = typing.cast(
+                typing.Optional[PositionComponent],
+                get_component_of_entity(ecdb=ecdb, entity=entity, component_type=PositionComponent),
+            )
+            assert position_component is not None
 
-        y_axis = position_component.y_axis + velocity_component.y_axis
-        x_axis = position_component.x_axis + velocity_component.x_axis
+            velocity_component = typing.cast(
+                typing.Optional[VelocityComponent],
+                get_component_of_entity(ecdb=ecdb, entity=entity, component_type=VelocityComponent),
+            )
+            assert velocity_component is not None
 
-        new_position_component = evolve(position_component, y_axis=y_axis, x_axis=x_axis)
-        ecdb = add_component(ecdb=ecdb, entity=entity, component=new_position_component)
+            y_axis = position_component.y_axis + velocity_component.y_axis
+            x_axis = position_component.x_axis + velocity_component.x_axis
 
-    return ecdb
+            new_position_component: PositionComponent = evolve(position_component, y_axis=y_axis, x_axis=x_axis)
+            ecdb = add_component(ecdb=ecdb, entity=entity, component=new_position_component)
+            ecdb = add_component(ecdb=ecdb, entity=entity, component=MovementSystem.ZERO_VELOCITY_COMPONENT)
 
-
-RANDOM_VALUE_TO_YX = {
-    0: (0, 1),
-    1: (1, 0),
-    2: (0, -1),
-    3: (-1, 0),
-}
+        return ecdb, SystemFeedback.NoFeedback
 
 
-def process_enemy_ai_system(
-    *, system: EnemyAISystem, ecdb: EntityComponentDatabase[RogueComponentUnion]
-) -> EntityComponentDatabase[RogueComponentUnion]:
-    assert system  # Hack to make 'system' variable marked as used
+@attr.s(frozen=True, kw_only=True)
+class EnemyAISystem(ReturnEntityComponentDatabaseTrait):
+    @classmethod
+    def create(cls) -> "EnemyAISystem":
+        return cls()
 
-    for entity in query_entities(ecdb=ecdb, query_function=is_enemy):
-        random_value = random.randint(0, len(RANDOM_VALUE_TO_YX) - 1)
-        y_axis, x_axis = RANDOM_VALUE_TO_YX[random_value]
+    RANDOM_VALUE_TO_YX = {
+        0: (0, 1),
+        1: (1, 0),
+        2: (0, -1),
+        3: (-1, 0),
+    }
 
-        velocity_component = create_velocity_component(y_axis=y_axis, x_axis=x_axis)
-        ecdb = add_component(ecdb=ecdb, entity=entity, component=velocity_component)
+    def __call__(
+        self, *, ecdb: EntityComponentDatabase[RogueComponentUnion]
+    ) -> typing.Tuple[EntityComponentDatabase[RogueComponentUnion], SystemFeedback]:
+        for entity in query_entities(ecdb=ecdb, query_function=is_enemy):
+            random_value = random.randint(0, len(EnemyAISystem.RANDOM_VALUE_TO_YX) - 1)
+            y_axis, x_axis = EnemyAISystem.RANDOM_VALUE_TO_YX[random_value]
 
-    return ecdb
+            velocity_component = create_velocity_component(y_axis=y_axis, x_axis=x_axis)
+            ecdb = add_component(ecdb=ecdb, entity=entity, component=velocity_component)
+
+        return ecdb, SystemFeedback.NoFeedback
+
+
+@attr.s(frozen=True, kw_only=True)
+class PygameHeroControlSystem(YieldEntityComponentDatabaseTrait):
+    @classmethod
+    def create(cls) -> "PygameHeroControlSystem":
+        return cls()
+
+    def __call__(
+        self, *, ecdb: EntityComponentDatabase[RogueComponentUnion]
+    ) -> typing.Generator[typing.Tuple[EntityComponentDatabase[RogueComponentUnion], SystemFeedback], None, None]:
+
+        while True:
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    yield ecdb, SystemFeedback.QuitGame
+
+                if event.type != pygame.KEYDOWN:
+                    yield ecdb, SystemFeedback.IgnorePygameEvent
+                else:
+                    hero = first(query_entities(ecdb=ecdb, query_function=is_hero))
+                    hero_velocity_y, hero_velocity_x = 0, 0
+
+                    if event.key == pygame.K_LEFT:
+                        hero_velocity_x = -1
+                    elif event.key == pygame.K_RIGHT:
+                        hero_velocity_x = 1
+                    elif event.key == pygame.K_UP:
+                        hero_velocity_y = -1
+                    elif event.key == pygame.K_DOWN:
+                        hero_velocity_y = 1
+                    elif event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        yield ecdb, SystemFeedback.QuitGame
+
+                    ecdb = add_component(
+                        ecdb=ecdb,
+                        entity=hero,
+                        component=create_velocity_component(y_axis=hero_velocity_y, x_axis=hero_velocity_x),
+                    )
+                    yield ecdb, SystemFeedback.NoFeedback
+
+
+SystemUnion = typing.Union[PygcurseRenderSystem, MovementSystem, EnemyAISystem, PygameHeroControlSystem]
 
 
 def process_system(
     *, system: SystemUnion, ecdb: EntityComponentDatabase[RogueComponentUnion]
-) -> EntityComponentDatabase[RogueComponentUnion]:
-    if isinstance(system, PygcurseRenderSystem):
-        process_pygcurse_render_system(system=system, ecdb=ecdb)
-    elif isinstance(system, MovementSystem):
-        ecdb = process_movement_system(system=system, ecdb=ecdb)
-    elif isinstance(system, EnemyAISystem):
-        ecdb = process_enemy_ai_system(system=system, ecdb=ecdb)
+) -> typing.Tuple[EntityComponentDatabase[RogueComponentUnion], SystemFeedback]:
+    if isinstance(system, DoNotChangeEntityComponentDatabaseTrait):
+        feedback = system(ecdb=ecdb)
+    elif isinstance(system, ReturnEntityComponentDatabaseTrait):
+        ecdb, feedback = system(ecdb=ecdb)
+    elif isinstance(system, YieldEntityComponentDatabaseTrait):
+        output = next(system(ecdb=ecdb))
+        ecdb, feedback = output
     else:
-        raise ValueError(f"Unsupported System {type(system)}!")
-    return ecdb
+        raise ValueError(f"System of type {type(system)} does not support any of the system traits!")
+    return ecdb, feedback
 
 
 def process_systems(
     *, ecdb: EntityComponentDatabase[RogueComponentUnion], systems: Systems[SystemUnion]
-) -> EntityComponentDatabase[RogueComponentUnion]:
+) -> typing.Tuple[EntityComponentDatabase[RogueComponentUnion], SystemFeedback]:
+
+    old_ecdb = ecdb
+    feedback = SystemFeedback.NoFeedback
+
     for system in get_systems(systems=systems):
-        ecdb = process_system(system=system, ecdb=ecdb)
-    return ecdb
+        ecdb, feedback = process_system(system=system, ecdb=ecdb)
+
+        if feedback in {SystemFeedback.IgnorePygameEvent, SystemFeedback.QuitGame}:
+            return old_ecdb, feedback
+
+    return ecdb, feedback
