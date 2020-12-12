b830af64622f21494d7acb132d1f7aebd2fc06e0
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2020-12-12 03:14:44 -0500

    Render initial state of the game

--- rogue/cli/rogue_pygcurse.py
+++ rogue/cli/rogue_pygcurse.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import pathlib
 
 from rogue.io.loaders import load_rogue_ecdb_from_input_yaml
@@ -14,25 +16,25 @@ from rogue.systems import (
 from rogue.exceptions import QuitGameException, IgnoreTimeStepException
 
 
-def create_rogue_systems(*, window_height: int, window_width: int) -> Systems[SystemUnion]:
+def create_rogue_systems(*, window_height: int, window_width: int) -> Tuple[Systems[SystemUnion], PygcurseRenderSystem]:
 
     systems: Systems[SystemUnion] = create_systems()
     systems = add_system(systems=systems, priority=0, system=PygameHeroControlSystem.create())
     systems = add_system(systems=systems, priority=0, system=EnemyAISystem.create())
     systems = add_system(systems=systems, priority=1, system=CollisionDetectionSystem.create())
     systems = add_system(systems=systems, priority=2, system=MovementSystem.create())
-    systems = add_system(
-        systems=systems,
-        priority=3,
-        system=PygcurseRenderSystem.create_from_height_and_width(height=window_height, width=window_width),
-    )
-    return systems
+    render_system = PygcurseRenderSystem.create_from_height_and_width(height=window_height, width=window_width)
+    systems = add_system(systems=systems, priority=3, system=render_system,)
+    return systems, render_system
 
 
 def rogue_pygcurse(*, input_file_name: pathlib.Path, window_height: int, window_width: int) -> None:
 
     ecdb = load_rogue_ecdb_from_input_yaml(input_file_name=input_file_name)
-    systems = create_rogue_systems(window_height=window_height, window_width=window_width)
+    systems, render_system = create_rogue_systems(window_height=window_height, window_width=window_width)
+
+    # Render the initial state
+    render_system(ecdb=ecdb)
 
     while True:
         try:
